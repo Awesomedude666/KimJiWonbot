5bf7887c3d3c56dc866e6617c67c184bd4c1ad1b
Awesomedude666 <141739327+Awesomedude666@users.noreply.github.com>
2023-08-16 02:15:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -930,7 +930,7 @@ async def shortlink(bot, message):
         return await message.reply(f"You are anonymous admin. Turn off anonymous admin and try again this command")
     chat_type = message.chat.type
     if chat_type == enums.ChatType.PRIVATE:
-        return await message.reply_text(f"<b>Hey {message.from_user.mention}, This command only works on groups !\n\n<u>Follow These Steps to Connect Shortener:</u>\n\n1. Add Me in Your Group with Full Admin Rights\n\n2. After Adding in Grp, Set your Shortener\n\nSend this command in your group\n\n—> /shortlink ""{your_shortener_website_name} {your_shortener_api}\n\n#Sample:-\n/shortlink OnePageLink.in 807085265f74bb78d42c3c5994d9429c1edd5ac8\n\nThat's it!!! Enjoy Earning Money 💲\n\n[[[ Trusted Earning Site - https://bit.ly/OnePageLink ]]]\n\nIf you have any Doubts, Feel Free to Ask me - @Prince0_66\n\n(Puriyala na intha id la message pannunga - @Prince0_66)</b>")
+        return await message.reply_text(f"<b>Hey {message.from_user.mention}, This command only works on groups !\n\n<u>Follow These Steps to Connect Shortener:</u>\n\n1. Add Me in Your Group with Full Admin Rights\n\n2. After Adding in Grp, Set your Shortener\n\nSend this command in your group\n\n—> /shortlink ""{your_shortener_website_name} {your_shortener_api}\n\n#Sample:-\n/shortlink OnePageLink.in 807085265f74bb78d42c3c5994d9429c1edd5ac8\n\nThat's it!!! Enjoy Earning Money 💲\n\n[[[ Trusted Earning Site - https://bit.ly/OnePageLink ]]]\n\nIf you have any Doubts, Feel Free to Ask me - @D4Devendar\n\n(Puriyala na intha id la message pannunga - @D4Devendar)</b>")
     elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         grpid = message.chat.id
         title = message.chat.title
