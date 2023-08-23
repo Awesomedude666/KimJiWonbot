604f1c5a35556045448b26bd3b044ff513b17ae7
Awesomedude666 <141739327+Awesomedude666@users.noreply.github.com>
2023-08-24 01:36:37 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,7 +4,7 @@ class script(object):
 ɪ ᴀᴍ ᴘᴏᴡᴇʀ ғᴜʟʟ ᴀᴜᴛᴏ ғɪʟᴛᴇʀ ʙᴏᴛ
 ɪ ᴀᴍ ᴘᴍ sʜᴏʀᴛɴᴇʀ-ʟɪɴᴋ ʙᴏᴛ
 
-ᴍᴀɴᴛᴀɪɴᴇᴅ ʙʏ : <a href="https://t.me/Tonystark1M">Developer</a></b>Tony Stark</b>"""
+ᴍᴀɴᴛᴀɪɴᴇᴅ ʙʏ : <a href="https://t.me/Tonystark1M">Developer</a></b></b>"""
 
     
     HELP_TXT = """<b>Hᴇʏ {}
