e851659d0074495ff5dc5f537c869c71d4bde765
Devendar Reddy <141739327+Awesomedude666@users.noreply.github.com>
2024-07-12 11:19:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '28323011'))
 API_HASH = environ.get('API_HASH', '6304897bc339ed76d768a3e559d8cce7')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6305445707:AAEek8MS6R9jXspxgExXSdvyvHT8qiJuw4s")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7017675414:AAFugJsX4r66pYSRsBluOcLym9abbmA6u-Q")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -43,8 +43,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://uniqueuser666:mongotest@awesomedude.mz8z48f.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://uniqueuser666:znWxle3vXSfHinS6@cluster0.4xuo5ek.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_filesg')
 
 # Others
@@ -58,13 +58,13 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
 PORT = environ.get("PORT", "8080")
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/awesomedudegroup')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/kim_ji_won_drama_group')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/awesomedudechannel')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/awesomedudechannel1/3')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 MSG_ALRT = environ.get('MSG_ALRT', 'Wʜᴀᴛ Aʀᴇ Yᴏᴜ Lᴏᴏᴋɪɴɢ Aᴛ ?')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001654236125'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/awesomedudegroup')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002245628524'))
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/kim_ji_won_drama_group')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "False")), False)
 AUTO_FFILTER = is_enabled((environ.get('AUTO_FFILTER', "True")), True)
